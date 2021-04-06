2921804f0ecee0d45b1f48ff78d8de57c7aa0c59
Mohamed Hassan <mohamed.h.khalifa@gmail.com>
2021-04-06 06:51:24 +0200

    fetching products logic

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,9 +1,33 @@
-import React from "react";
-import '../UIKit/css/Pages.css'
+import React, { useState, useEffect } from "react";
+import useFetch from "../Hooks/useFetch";
+import Product from "./Product";
+import Loader from "../UIKit/Loader";
+import '../UIKit/css/Pages.css';
+import '../UIKit/css/Products.css';
 
 export default function Products(props) {
+  const [products, setProducts] = useState([]);
+  const { get, isLoading } = useFetch("https://react-tutorial-demo.firebaseio.com/");
+
+  useEffect(() => {
+    get("supermarket.json")
+    .then(data => {
+      setProducts(data);
+    })
+    .catch(err => console.log(err))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return <div className="products-layout">
     <h1>Products</h1>
     <p>Take a look at our products</p>
+    <div className="products-grid">
+      { isLoading && <Loader /> }
+      {
+        products.map(product => {
+          return <Product key={product.id} details={product} />
+        })
+      }
+    </div>
   </div>;
 }
---
+++ src/Hooks/useFetch.js
@@ -0,0 +1,51 @@
+import {useState} from 'react';
+
+export default function useFetch(baseurl) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const get = function (url) {
+    setIsLoading(true);
+    return new Promise((res, rej) => {
+      fetch(baseurl + url)
+      .then(response => response.json())
+      .then(data => {
+        if (!data) {
+          setIsLoading(false);
+          return rej("Error in resposne data");
+        }
+        return res(data);
+      })
+      .catch(err => rej(err))
+      .finally(() => {
+        setIsLoading(false);
+      })
+    });
+  }
+
+  const post = function(url, body) {
+    setIsLoading(true);
+    return new Promise((res, rej) => {
+      fetch(baseurl + url, {
+        method: 'post',
+        headers: {
+          'content-type': 'application-json'
+        },
+        body: body
+      })
+      .then(response => response.json())
+      .then(data => {
+        if (!data) {
+          setIsLoading(false);
+          rej("Error in response data");
+        }
+        res(data);
+      })
+      .catch(err => rej(err))
+      .finally(() => {
+        setIsLoading(false)
+      })
+    });
+  }
+
+  return { get, post, isLoading};
+}
