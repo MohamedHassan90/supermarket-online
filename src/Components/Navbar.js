7dd3c676ab518dba11e1b1ccee4df53e8f14302c
Mohamed Hassan <mohamed.h.khalifa@gmail.com>
2021-04-14 10:31:32 +0200

    Added Dark Mode option and css Fixes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { AppContext } from "../Context/AppContext";
 import AppTitle from "./AppTitle";
@@ -6,12 +6,37 @@ import "../UIKit/css/Nav.css";
 import "../UIKit/css/Button.css";
 
 export default function Navbar(props) {
+  const [theme, setTheme] = useState("Dark");
   const { getTotalQuantity } = useContext(AppContext);
+
+  useEffect(() => {
+    let bodyEl = document.querySelector('body');
+    if (theme === "Light") {
+      bodyEl.classList.add('dark');
+    } else {
+      bodyEl.classList.remove('dark');
+    }
+  }, [theme]);
+
+  function toggleTheme() {
+    if (theme === 'Light') {
+      setTheme("Dark");
+    } else {
+      setTheme("Light");
+    }
+  }
 
   return (
     <nav className="navbar">
       <AppTitle />
       <ul>
+        <li className="nav-item">
+          <label className="switch">
+            <input type="checkbox" onChange={toggleTheme} />
+            <span className="slider round"></span>
+          </label>
+          <label>Dark</label>
+        </li>
         <li className="nav-item">
           <NavLink exact to="/" activeClassName="active">Home</NavLink>
         </li>
